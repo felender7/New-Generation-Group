aad3c11cd1f87c69fd8a5b288ff72299dc5247da
vee <vutshilamaringa@gmail.com>
2018-05-18 16:11:56 +0200

    added mamangement consultion page

---
+++ app/controllers/company_controller.rb
@@ -0,0 +1,5 @@
+class CompanyController < ApplicationController
+  def management_consulting
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get'about', to: 'about#index'
-
+  resources :company do
+  collection do
+       get'management_consulting'
+      end
+    end
   root 'home#index'
 end
