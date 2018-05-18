bd99d2a747ec62939bf5319a59819eb2832189d3
vee <vutshilamaringa@gmail.com>
2018-05-18 08:23:37 +0200

    fixed css

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Put this at the end of your config/routes.rb
+
 end
