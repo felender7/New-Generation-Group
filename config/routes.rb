d5ef193e29624404f7d412cdc4e656ad89c61a6a
vee <vutshilamaringa@gmail.com>
2018-05-22 01:42:35 +0200

    added inteview page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get'/about', to: 'about#index'
-
+ get'/about', to: 'about#index'
  get'/management_consulting', to:'company#management_consulting'
  get'/placements' , to:'company#placements'
+ get'/intelliview', to:'company#intelliview'
 
   root 'home#index'
 end
